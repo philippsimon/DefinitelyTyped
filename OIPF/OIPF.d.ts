aafefea8d6aea1477e691438af877ad619f10e37
philippsimon <github@philippsimon.de>
2016-01-04 10:24:14 +0100

    first version OIPF

---
+++ OIPF/OIPF.d.ts
@@ -0,0 +1,24 @@
+interface OIPFAVControlElement extends HTMLObjectElement {
+  error: number;
+  stop: () => void;
+  onPlayStateChange: () => void;
+  playPosition: number;
+  playState: number;
+  playTime: number;
+  seek: (pos: number) => void;
+  play: (speed: number) => void;
+}
+
+interface OIPFApplication {}
+
+interface OIPFApplicationCollection {}
+
+interface OIPFApplicationManager {
+  onLowmemory: () => void;
+  onApplicationLoaded: (application: OIPFApplication) => void;
+  onApplicationUnloaded: (application: OIPFApplication) => void;
+  getApplicationVisualizationMode: () => number;
+  getOwnerApplication: (document: Document) => OIPFApplication;
+  getChildApplications: (application: OIPFApplication) => OIPFApplicationCollection;
+  gc: () => void;
+}
